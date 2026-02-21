1899d9b4d94c2b0f67f67848fc83e94350038ec8
Rizaldy Alfarez <zaldyalfarez@gmail.com>
2026-02-21 19:54:31 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/arsitek-kode",
+  // base: import.meta.env.VITE_BASE_PATH || "/arsitek-kode",
 });
